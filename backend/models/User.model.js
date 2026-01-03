2904f87976cd3389b39cce590cc66dd5a816cada
Kunal23237 <rathodkunal371@gmail.com>
2026-01-03 14:55:48 +0530

    feat: update user and leave models

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -51,12 +51,18 @@ const userSchema = new mongoose.Schema(
 
 // Hash password before saving
 // Hash password before saving
+// Hash password before saving
+// Hash password before saving
 userSchema.pre('save', async function () {
     // Only hash if password is modified
     if (!this.isModified('password')) return;
 
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+    } catch (error) {
+        throw new Error(error);
+    }
 });
 
 
